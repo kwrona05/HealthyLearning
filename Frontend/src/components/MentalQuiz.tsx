e0597ce5d50619fa71b27c9ebaf78bdcb5f5c22f
kwrona05 <kwrona05@gmail.com>
2025-02-05 20:58:42 +0100

    Creating quiz form

--- Frontend/src/components/MentalQuiz.tsx
+++ Frontend/src/components/MentalQuiz.tsx
@@ -1,32 +1,11 @@
 import { useState } from "react";
 const MentalQuiz = () => {
-  const [answers, setAnswers] = useState({
-    stress: "",
-    sleep: "",
-  });
+  const [selectedAnswer, setSelectedAnswer] = useState([]);
 
-  const [recomendations, setRecomendations] = useState("");
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setAnswers((prevAnswers) => ({
-      ...prevAnswers,
-      [name]: value,
-    }));
+  const handleChange = (event) => {
+    setSelectedAnswer(event.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let result = "";
-
-    if (answers.stress === "often") {
-      result +=
-        "You have a lot of stress. You should try for example breathing methods.";
-    }
-
-    if (answers.sleep === "less than 6 hours") {
-      result += "That's not a lot... You should change your week.";
-    }
-  };
+  return <div className="styles.quizContainer"></div>;
 };
 export default MentalQuiz;
