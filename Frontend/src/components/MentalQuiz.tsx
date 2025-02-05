12c62a3064092bb9d8d93c63dc921feb577cabcf
kwrona05 <kwrona05@gmail.com>
2025-02-05 21:15:51 +0100

    Add questions

--- Frontend/src/components/MentalQuiz.tsx
+++ Frontend/src/components/MentalQuiz.tsx
@@ -6,6 +6,48 @@ const MentalQuiz = () => {
     setSelectedAnswer(event.target.value);
   };
 
-  return <div className="styles.quizContainer"></div>;
+  return (
+    <div className="styles.quizContainer">
+      <label className="styles.questionLabel">
+        <p>I have a lot of stress</p>
+        <input type="radio" name="stress">
+          Yes
+        </input>
+        <input type="radio" name="stress">
+          Maybe
+        </input>
+        <input type="radio" name="stress">
+          No
+        </input>
+      </label>
+      <label className="styles.questionLabel">
+        <p>How much sleep do you have</p>
+        <input type="radio" name="sleep">
+          +8 hours
+        </input>
+        <input type="radio" name="stress">
+          6-8 hours
+        </input>
+        <input type="radio" name="stress">
+          less than 6 hours
+        </input>
+      </label>
+      <label className="styles.questionLabel">
+        <p>Have you got problems in school?</p>
+        <input type="radio" name="school">
+          Yes, I can't focus
+        </input>
+        <input type="radio" name="school">
+          Yes, I have problem with science subjects
+        </input>
+        <input type="radio" name="school">
+          Yes, I have problem with humanities subjects
+        </input>
+        <input type="radio" name="school">
+          Yes, I have problems with my memory
+        </input>
+      </label>
+    </div>
+  );
 };
 export default MentalQuiz;
