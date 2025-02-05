1be026a6efc032aeb91b7e1b82ff2cfc3d92a295
kwrona05 <kwrona05@gmail.com>
2025-02-05 14:53:32 +0100

    Set logic for quiz

--- Frontend/src/components/MentalQuiz.tsx
+++ Frontend/src/components/MentalQuiz.tsx
@@ -1,3 +1,32 @@
 import { useState } from "react";
-const MentalQuiz = () => {};
+const MentalQuiz = () => {
+  const [answers, setAnswers] = useState({
+    stress: "",
+    sleep: "",
+  });
+
+  const [recomendations, setRecomendations] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let result = "";
+
+    if (answers.stress === "often") {
+      result +=
+        "You have a lot of stress. You should try for example breathing methods.";
+    }
+
+    if (answers.sleep === "less than 6 hours") {
+      result += "That's not a lot... You should change your week.";
+    }
+  };
+};
 export default MentalQuiz;
